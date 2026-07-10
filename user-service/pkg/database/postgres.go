08b45ccbc6126e6696bac059975c8c7b58d87e74
Max Grigoriev <m.grigoryev@8cell.tech>
2026-07-10 13:29:00 +0300

    add: init db

---
+++ user-service/pkg/database/postgres.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/anomalyco/hookah-store/user-service/internal/config"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func NewDB(cfg *config.DBConfig) (*sqlx.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DBName, cfg.SSLMode,
+	)
+
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("connect to db: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("ping db: %w", err)
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+
+	return db, nil
+}
