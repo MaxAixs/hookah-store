7614a4aa605419df00e690ed0d844d6c333c2548
Max Grigoriev <m.grigoryev@8cell.tech>
2026-07-13 20:49:24 +0300

    add: init struct outBoxEvent

--- user-service/internal/models/db.go
+++ user-service/internal/models/db.go
@@ -14,3 +14,22 @@ type User struct {
 	CreatedAt    time.Time `db:"created_at"`
 	UpdatedAt    time.Time `db:"updated_at"`
 }
+
+type OutboxEvent struct {
+	ID        uuid.UUID `db:"id"`
+	Topic     string    `db:"topic"`
+	Key       string    `db:"key"`
+	Payload   []byte    `db:"payload"`
+	CreatedAt time.Time `db:"created_at"`
+	Published bool      `db:"published"`
+}
+
+func NewOutBoxEvent(topic string, key string, payload []byte) *OutboxEvent {
+	return &OutboxEvent{
+		ID:        uuid.New(),
+		Topic:     topic,
+		Key:       key,
+		Payload:   payload,
+		CreatedAt: time.Now(),
+	}
+}
