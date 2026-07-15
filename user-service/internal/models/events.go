00ae0f31066ffbde4a15e87412f70a5c1c9f12c1
Max Grigoriev <m.grigoryev@8cell.tech>
2026-07-15 14:36:05 +0300

    add: AuthEventType with SignUp and ResetPassword constants

--- user-service/internal/models/events.go
+++ user-service/internal/models/events.go
@@ -6,8 +6,16 @@ import (
 	"github.com/google/uuid"
 )
 
+type AuthEventType string
+
+const (
+	AuthEventSignUp        AuthEventType = "sign_up"
+	AuthEventResetPassword AuthEventType = "reset_password"
+)
+
 type UserAuthEvent struct {
-	UserID    uuid.UUID `json:"user_id"`
-	Email     string    `json:"email"`
-	TimeStamp time.Time `json:"timestamp"`
+	UserID    uuid.UUID     `json:"user_id"`
+	Email     string        `json:"email"`
+	EventType AuthEventType `json:"event_type"`
+	TimeStamp time.Time     `json:"timestamp"`
 }
