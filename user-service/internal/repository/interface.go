76ebaa2880580907c4fe5111a73b2a1c203b7ae4
Max Grigoriev <m.grigoryev@8cell.tech>
2026-07-15 13:57:58 +0300

    ref: сhange places interface

--- user-service/internal/repository/interface.go
+++ user-service/internal/repository/interface.go
@@ -8,17 +8,17 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-type OutBoxRepository interface {
-	SaveEvent(ctx context.Context, tx *sqlx.Tx, event *models.OutboxEvent) error
-	FetchUnpublishedEvents(limit int) ([]models.OutboxEvent, error)
-	MarkPublishedEvents(ctx context.Context, id uuid.UUID) error
-}
-
 type UserRepository interface {
 	Create(ctx context.Context, tx *sqlx.Tx, user *models.User) error
 	GetByID(ctx context.Context, id uuid.UUID) (*models.User, error)
 	Update(ctx context.Context, user *models.User) error
 	Delete(ctx context.Context, id uuid.UUID) error
 	GetByEmail(ctx context.Context, email string) (*models.User, error)
-	UpdatePassword(ctx context.Context, email string, newPassword string) error
+	UpdatePassword(ctx context.Context, tx *sqlx.Tx, email string, newPassword string) error
+}
+
+type OutBoxRepository interface {
+	SaveEvent(ctx context.Context, tx *sqlx.Tx, event *models.OutboxEvent) error
+	FetchUnpublishedEvents(limit int) ([]models.OutboxEvent, error)
+	MarkPublishedEvents(ctx context.Context, id uuid.UUID) error
 }
